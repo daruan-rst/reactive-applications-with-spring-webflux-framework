bc7df2acf4db52b28988f2c2fee2aa8a0d945b8c
daruan <daruan.rst@gmail.com>
2026-04-19 23:23:40 -0300

    @RestController: Creating and configure the RestController class

---
+++ src/main/java/com/appsdevelloperblog/reactive/ws/users/infrastructure/presentation/UserController.java
@@ -0,0 +1,9 @@
+package com.appsdevelloperblog.reactive.ws.users.infrastructure.presentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+}
