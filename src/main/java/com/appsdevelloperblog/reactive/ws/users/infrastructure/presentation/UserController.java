14973adfd4195478c9d1d2ad44d9ac398aa99677
daruan <daruan.rst@gmail.com>
2026-04-20 19:35:52 -0300

    @RequestBody: Reading HTTP Request Body

--- src/main/java/com/appsdevelloperblog/reactive/ws/users/infrastructure/presentation/UserController.java
+++ src/main/java/com/appsdevelloperblog/reactive/ws/users/infrastructure/presentation/UserController.java
@@ -1,15 +1,17 @@
 package com.appsdevelloperblog.reactive.ws.users.infrastructure.presentation;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @PostMapping
-    public void createUser(){
+    public void createUser(@RequestBody Mono<CreateUserRequest> createUserRequest){
 
 
     }
