6dcc79742a86ea44e71818d0b8d136ce0fc704e3
daruan <daruan.rst@gmail.com>
2026-05-09 23:45:25 -0300

    Formatting Bean validation errors in Spring WebFlux application

--- src/main/java/com/appsdevelloperblog/reactive/ws/users/infrastructure/GlobalExceptionHandler.java
+++ src/main/java/com/appsdevelloperblog/reactive/ws/users/infrastructure/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
 package com.appsdevelloperblog.reactive.ws.users.infrastructure;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -13,6 +17,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DuplicateKeyException.class)
     public Mono<ErrorResponse> handleDuplicateKeyException(DuplicateKeyException exception){
         return Mono.just(ErrorResponse.builder(exception, HttpStatus.CONFLICT, exception.getMessage() ).build());
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public Mono<ErrorResponse> handleWebExchangeBindException(WebExchangeBindException exception){
+        String errorMessage = exception.getBindingResult().getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+        return Mono.just(ErrorResponse.builder(exception, HttpStatus.BAD_REQUEST, errorMessage ).build());
     }
 
     @ExceptionHandler(Exception.class)
