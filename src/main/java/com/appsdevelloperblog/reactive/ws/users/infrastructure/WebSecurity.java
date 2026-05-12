246382f95ccedaee8b0b4177290b74a36d547d26
daruan <daruan.rst@gmail.com>
2026-05-12 20:37:10 -0300

    Granting access to public API endpoints in reactive Spring WebFlux application

--- src/main/java/com/appsdevelloperblog/reactive/ws/users/infrastructure/WebSecurity.java
+++ src/main/java/com/appsdevelloperblog/reactive/ws/users/infrastructure/WebSecurity.java
@@ -2,6 +2,7 @@ package com.appsdevelloperblog.reactive.ws.users.infrastructure;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -12,6 +13,15 @@ public class WebSecurity {
 
     @Bean
     SecurityWebFilterChain httpSecurityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.POST,"/users")
+                .permitAll()
+                .anyExchange()
+                .authenticated())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+
+
 
     }
 
