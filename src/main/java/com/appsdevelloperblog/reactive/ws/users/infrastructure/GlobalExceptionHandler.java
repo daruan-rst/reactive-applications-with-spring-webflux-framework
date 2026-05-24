d5e43109c70ab33fb100380e2ead9ef404eb2a55
daruan-rst <daruan.rst@gmail.com>
2026-05-23 21:51:30 -0300

    Global Error Handling - Handle Unsuccessful User Authentication

--- src/main/java/com/appsdevelloperblog/reactive/ws/users/infrastructure/GlobalExceptionHandler.java
+++ src/main/java/com/appsdevelloperblog/reactive/ws/users/infrastructure/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.appsdevelloperblog.reactive.ws.users.infrastructure;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -30,5 +31,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public Mono<ErrorResponse> handleGeneralException(Exception exception){
         return Mono.just(ErrorResponse.builder(exception, HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage() ).build());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public Mono<ErrorResponse> handleBadCredentialsException(BadCredentialsException exception) {
+        return Mono.just(ErrorResponse.builder(exception, HttpStatus.UNAUTHORIZED, exception.getMessage()).build());
     }
 }
