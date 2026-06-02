5fbab49173a91ebd12cd3435f66c2bc3332296f6
daruan-rst <daruan.rst@gmail.com>
2026-06-01 23:59:24 -0300

    Extracting Subject claim from JWT token

--- src/main/java/com/appsdevelloperblog/reactive/ws/users/infrastructure/WebSecurity.java
+++ src/main/java/com/appsdevelloperblog/reactive/ws/users/infrastructure/WebSecurity.java
@@ -1,14 +1,17 @@
 package com.appsdevelloperblog.reactive.ws.users.infrastructure;
 
+import com.appsdevelloperblog.reactive.ws.users.service.JwtService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -16,7 +19,9 @@ public class WebSecurity {
 
     @Bean
     SecurityWebFilterChain httpSecurityWebFilterChain(ServerHttpSecurity http,
-                                                      ReactiveAuthenticationManager reactiveAuthenticationManager) {
+                                                      ReactiveAuthenticationManager reactiveAuthenticationManager,
+                                                      JwtService jwtService) {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtService);
         return http
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers(HttpMethod.POST,"/users").permitAll()
@@ -26,6 +31,8 @@ public class WebSecurity {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
                 .authenticationManager(reactiveAuthenticationManager)
+                .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
                 .build();
 
     }
